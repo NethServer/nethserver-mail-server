5d05a029f84a3cc2bf3dc1ef2f23e4d8f2591f19
Davide Principi <davide.principi@nethesis.it>
2012-06-15 15:40:45 +0200

    user-create event: Implemented creation of user default pseudonyms. Refs #1221 - Pseudonym/account synchronization

--- NethServer/MailServer.pm
+++ NethServer/MailServer.pm
@@ -8,8 +8,17 @@ package NethServer::MailServer;
 
 use esmith::AccountsDB;
 use esmith::DomainsDB;
+use Text::Unidecode;
 
-=pod
+=head1 NethServer::MailServer package
+
+Encapsulate the MailServer domain logic.
+
+Copyright (C) 2012 Nethesis srl
+
+=head2 new()
+
+Create a new MailServer object
 
 =cut
 sub new 
@@ -20,13 +29,15 @@ sub new
 
     bless $self, $class;
 
-    $self->{AccountsDB} = esmith::AccountsDB->open_ro();
-    $self->{DomainsDB} = esmith::DomainsDB->open_ro();
+    $self->{AccountsDb} = esmith::AccountsDB->open();
+    $self->{DomainsDb} = esmith::DomainsDB->open();
 
     return $self;
 }
 
-=pod
+=head2 ->getMailboxes()
+
+Return the list of currently active mailboxes
 
 =cut
 sub getMailboxes()
@@ -44,7 +55,9 @@ sub getMailboxes()
     return keys %mailboxes;
 }
 
-=pod
+=head2 ->getAliases()
+
+Return an hash describing the alias => mailbox association
 
 =cut
 sub getAliases()
@@ -52,17 +65,17 @@ sub getAliases()
     my $self = shift;
     my %aliasMap = ();
 
-    foreach my $record ($self->{AccountsDB}->pseudonyms()) {
+    foreach my $record ($self->{AccountsDb}->pseudonyms()) {
 	my $pseudonym = $record->key;
 	my $account = $record->prop('Account');
 	my $domain = $pseudonym;
 
-	my $accountRecord = $self->{AccountsDB}->get($account);
+	my $accountRecord = $self->{AccountsDb}->get($account);
 
 	# Skip the pseudonym if the reffered account is not 
 	# enabled.
 	if(! defined($accountRecord) ) {
-	    warn "Account `$account` not found";
+	    $self->{debug} && warn "Account `$account` not found";
 	    next;
 	} elsif($accountRecord->prop('type') eq 'user'
 	    &&  $accountRecord->prop('MailStatus') ne 'enabled') {
@@ -75,7 +88,7 @@ sub getAliases()
 	$domain =~ s/([^@]+@)//;
 
 	if(! $domain ) {
-	    warn "Found pseudonym key `$pseudonym` without \@domain suffix: skipped";
+	    $self->{debug} && warn "Found pseudonym key `$pseudonym` without \@domain suffix: skipped";
 	    next;
 	}
 
@@ -87,7 +100,64 @@ sub getAliases()
 }
 
 
+=head2 ->createUserPseudonyms($$)
+
+Create user pseudonyms according to our rules
+
+=cut
+sub createUserPseudonyms($) 
+{
+    my $self = shift;
+    my $username = shift;
+
+    my @domainList = $self->getDeliveryDomains();
+
+    my $userRecord = $self->{AccountsDb}->get($username);
+
+    if ( ! $userRecord ) {
+	return 0; # failure
+    }
+
+    my $firstName = lc(unidecode($userRecord->prop('FirstName')));
+    my $lastName = lc(unidecode($userRecord->prop('LastName')));
+
+    # Trim any whitespace character
+    $firstName =~ s/\s+//;
+    $lastName =~ s/\s+//;
+
+    foreach my $domain (@domainList) {
+	my $address1 = $firstName . '.' . $lastName . '@' . $domain;
+	my $props = {
+	    'type' => 'pseudonym',
+	    'Account' => $username,
+	    'ControlledBy' => 'system'
+	};
+
+	my $newRecord = $self->{AccountsDb}->new_record($address1, $props);
+
+	if( ! $newRecord) {
+	    $self->{debug} && warn ("Pseudonym ${address1} already exists");
+	}
+    }
+
+    return 1;
+}
 
 
+
+sub getDeliveryDomains() 
+{
+    my $self = shift;
+    # Fill the list of domains with Local or Remote Delivery type:
+    my @domainList = ();
+    foreach my $domainRecord ($self->{DomainsDb}->get_all_by_prop(type => 'domain')) {
+	my $deliveryType = $domainRecord->prop('TransportType');
+	if($deliveryType eq 'LocalDelivery'
+	    || $deliveryType eq 'RemoteDelivery') {
+	    push @domainList, $domainRecord->key;
+	}
+    }
+    return @domainList;
+}
 
 1;
