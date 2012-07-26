c5cf72bcf1bd3f80c24b5f22d278951353664991
Davide Principi <davide.principi@nethesis.it>
2012-07-26 17:06:42 +0200

    Mailbox UI mail module plugin: Quota form changes..

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Mail_Mailbox.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Mail_Mailbox.php
@@ -2,7 +2,8 @@
 
 $L['Mailbox_Title'] = 'Caselle di posta';
 
-$L['QuotaStatus_disabled_label'] = 'Spazio disco illimitato';
+$L['Disk space'] = 'Spazio disco';
+$L['QuotaStatus_disabled_label'] = 'Illimitato';
 $L['QuotaStatus_enabled_label'] = 'Applica quota';
 $L['Quota default size ${0}'] = 'Valore predefinito: ${0}';
 $L['Mailbox access protocols'] = 'Protocolli di accesso';
--- root/usr/share/nethesis/NethServer/Template/Mail/Mailbox.php
+++ root/usr/share/nethesis/NethServer/Template/Mail/Mailbox.php
@@ -12,16 +12,16 @@ echo $view->fieldset()->setAttribute('template', $T('Mailbox access protocols'))
     ->insert(
         $view->checkbox('TlsSecurity', 'optional')
         ->setAttribute('uncheckedValue', 'required')
-    )    
+    )
 ;
 
-echo $view->radioButton('QuotaStatus', 'disabled');
-echo $view->fieldsetSwitch('QuotaStatus', 'enabled', $view::FIELDSETSWITCH_EXPANDABLE)
-
-    ->insert(
-        $view->slider('QuotaDefaultSize', $view::SLIDER_ENUMERATIVE | $view::LABEL_ABOVE)
-        ->setAttribute('label', $T('Quota default size ${0}'))
-    )
+echo $view->fieldset()->setAttribute('template', $T('Disk space'))
+    ->insert($view->radioButton('QuotaStatus', 'disabled'))
+    ->insert($view->fieldsetSwitch('QuotaStatus', 'enabled', $view::FIELDSETSWITCH_EXPANDABLE)
+        ->insert(
+            $view->slider('QuotaDefaultSize', $view::SLIDER_ENUMERATIVE | $view::LABEL_ABOVE)
+            ->setAttribute('label', $T('Quota default size ${0}'))
+        ))
 ;
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
