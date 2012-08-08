bd610ac22ee063e7e83fbdfcca73139fb8306690
Davide Principi <davide.principi@nethesis.it>
2012-08-08 18:20:05 +0200

    Changed some translations..

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_User_Plugin_Mail.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_User_Plugin_Mail.php
@@ -2,12 +2,12 @@
 
 $L['MailStatus_label'] = 'Email';
 
-$L['MailQuotaType_label'] = 'Personalizza quota email';
+$L['MailQuotaType_label'] = 'Quota email personalizzata';
 $L['MailForwardStatus_label'] = 'Inoltro messaggi';
 $L['MailForwardAddress_label'] = 'Indirizzo email';
 $L['MailForwardKeepMessageCopy_label'] = 'Tieni una copia sul server';
 $L['Unlimited_quota'] = 'Illimitata';
-$L['MailSpamRetentionStatus_label'] = 'Personalizza conserva messaggi spam';
+$L['MailSpamRetentionStatus_label'] = 'Conserva messaggi spam personalizzata ';
 $L['Hold for ${0}'] = 'Conserva per ${0}';
 $L['${0} day'] = 'un giorno';
 $L['${0} days'] = '${0} giorni';
