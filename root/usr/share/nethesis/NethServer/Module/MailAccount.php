9011936b74db7103616804d996e6dc33d0f4d1a4
Davide Principi <davide.principi@nethesis.it>
2016-05-17 16:20:38 +0200

    Move "Mail Alias" tab to last position

--- root/usr/share/nethesis/NethServer/Module/MailAccount.php
+++ root/usr/share/nethesis/NethServer/Module/MailAccount.php
@@ -37,7 +37,8 @@ class MailAccount extends \Nethgui\Controller\TabsController
     {
         parent::initialize();
         $this->addChild(new \NethServer\Module\MailAccount\User());
-        $this->addChild(new \NethServer\Module\MailAccount\Pseudonym());
         $this->addChild(new \NethServer\Module\MailAccount\SharedMailbox());
+        $this->addChild(new \NethServer\Module\MailAccount\Pseudonym());
     }
+
 }
