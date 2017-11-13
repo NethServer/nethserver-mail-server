8e9f4f140037a665fa21ef343dc8a7a0fb5610ff
Davide Principi <davide.principi@nethesis.it>
2017-11-13 16:30:01 +0100

    Restrict catch all to local mailboxes
    Undeliverable messages are sent to a local mailbox only. Existing values
    are retained - remote addresses are still valid for alias domains. New
    domain records can select only local mailboxes.

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Mail_Domain_PlugTransport_LocalDelivery.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Mail_Domain_PlugTransport_LocalDelivery.php
@@ -9,3 +9,4 @@ $L['UnknownRecipientsActionDeliverMailbox_label'] = 'Deliver to';
 $L['UnknownRecipientsActionType_label'] = 'Accept unknown recipients';
 $L['Users_label'] = 'Users';
 $L['valid_catchall_mailbox_primary'] = 'A local mailbox identifier, like "root", or "username@${0}"';
+$L['SharedMailbox_selector_label'] = '${0} (shared mailbox)';
